304be54ec686813983dc816801c3cf0a4c9499b3
Mako0530 <zhongtiancheng79@gmail.com>
2021-02-04 21:20:45 +0900

    articles controller に変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root to: 'home#index'
+  root to: 'articles#index'
 
-  get '/' => 'home#index'
   get '/about' => 'home#about'
 end
