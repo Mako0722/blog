bc1e2a5060d6bc01f103cee821dd9a4f0ea816a4
Mako0530 <zhongtiancheng79@gmail.com>
2021-02-05 22:48:33 +0900

    DBのスキーマを修正

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_05_131348) do
+ActiveRecord::Schema.define(version: 2021_02_04_113854) do
 
   create_table "articles", force: :cascade do |t|
-    t.string "title"
-    t.text "content"
+    t.integer "user_id", null: false
+    t.string "title", null: false
+    t.text "content", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.integer "user_id"
     t.index ["user_id"], name: "index_articles_on_user_id"
   end
 
