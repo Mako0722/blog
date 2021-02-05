6489c06087d8a63882988090386e5bbc716f01fd
Mako0530 <zhongtiancheng79@gmail.com>
2021-02-06 00:31:56 +0900

    delegateを使用

--- app/models/user.rb
+++ app/models/user.rb
@@ -25,6 +25,8 @@ class User < ApplicationRecord
   has_many :articles, dependent: :destroy
   has_one :profile, dependent: :destroy
 
+  delegate :birthday, :gender, to: :profile, allow_nil: true
+
   def has_written?(article)
     articles.exists?(id: article.id)
   end
@@ -33,13 +35,6 @@ class User < ApplicationRecord
     profile&.nickname || self.email.split('@').first
   end
 
-  def birthday
-    profile&.birthday
-  end
-
-  def gender
-    profile&.gender
-  end	  
 
   def prepare_profile
     profile || build_profile
