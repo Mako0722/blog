9e345ef50ee850daf1ec908d2d23d0bd7a9fa92d
Mako0530 <zhongtiancheng79@gmail.com>
2021-02-20 21:48:12 +0900

    いいねの状態で表示を切り替える

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -22,11 +22,23 @@ require("@rails/actiontext")
 import $ from 'jquery'
 import axios from 'axios'
 
+const handleHeartDisplay = (hasLiked) => {
+  if (hasLiked) {
+    $('.active-heart').removeClass('hidden')
+  } else {
+    $('.inactive-heart').removeClass('hidden')
+  }
+}
+
+
+
 document.addEventListener('turbolinks:load', () => {
-  $('.article_title').on('click', () => {
-    axios.get('/')
-      .then((response) => {
-        console.log(response)
-      })
-  })
+  const dataset = $('#article-show').data()
+  const articleId = dataset.articleId
+
+  axios.get(`/articles/${articleId}/like`)
+    .then((response) => {
+      const hasLiked = response.data.hasLiked
+      handleHeartDisplay(hasLiked)
+    })
 })
