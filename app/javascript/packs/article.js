dcba69803e1ad69386c07e604e0527541e48176f
Mako0530 <zhongtiancheng79@gmail.com>
2021-02-22 04:11:15 +0900

    少しだけリファクタリング

--- app/javascript/packs/article.js
+++ app/javascript/packs/article.js
@@ -12,26 +12,32 @@ const handleHeartDisplay = (hasLiked) => {
   }
 }
 
+const handleCommentForm = () => {
+  $('.show-comment-form').on('click', () => {
+    $('.show-comment-form').addClass('hidden')
+    $('.comment-text-area').removeClass('hidden')
+  })
+}
 
+const appendNewComment = (comment) => {
+  $('.comments-container').append(
+    `<div class="article_comment"><p>${comment.content}</p></div>`
+  )
+}
 
 document.addEventListener('DOMContentLoaded', () => {
   const dataset = $('#article-show').data()
   const articleId = dataset.articleId
 
-  $('.show-comment-form').on('click', () => {
-    $('.show-comment-form').addClass('hidden')
-    $('.comment-text-area').removeClass('hidden')
-  })
-
   axios.get(`/articles/${articleId}/comments`)
     .then((response) => {
       const comments = response.data
       comments.forEach((comment) => {
-        $('.comments-container').append(
-          `<div class="article_comment"><p>${comment.content}</p></div>`
-        )
+        appendNewComment(comment)
       })
     })
+
+    handleCommentForm()
 
     $('.add-comment-button').on('click', () => {
     const content = $('#comment_content').val()
@@ -43,9 +49,8 @@ document.addEventListener('DOMContentLoaded', () => {
       })
         .then((res) => {
           const comment = res.data
-          $('.comments-container').append(
-            `<div class="article_comment"><p>${comment.content}</p></div>`
-          )
+          appendNewComment(comment)
+
           $('#comment_content').val('')
         })
     }
