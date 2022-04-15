ae0da8aa27163659421b3599f13adf8b43801705
Mako0530 <zhongtiancheng79@gmail.com>
2022-04-15 20:16:51 +0900

    不要なコメントを削除

--- src/public/user/signup_complete.php
+++ src/public/user/signup_complete.php
@@ -41,28 +41,3 @@ try {
     $_SESSION['user']['email'] = $email;
     Redirect::handler('./signup.php');
 }
-
-// session_start();
-// if (empty($password) || empty($confirmPassword)) $_SESSION['errors'][] = "パスワードを入力してください";
-
-// if ($password !== $confirmPassword) $_SESSION['errors'][] = "パスワードが一致しません";
-
-// if(!empty($_SESSION['errors'])){
-//     $_SESSION['user']['name'] = $name;
-//     $_SESSION['user']['email'] =  $email;
-//     redirect('./signup.php');
-// }
-
-// $userName = new UserName($name);
-// $userEmail = new Email($email);
-// $userPassword = new InputPassword($password);
-// $useCaseInput = new SignUpInput($name, $email, $password);
-// $useCase = new SignUpInteractor($useCaseInput);
-// $useCaseOutput = $useCase->handler();
-// if ($useCaseOutput->isSuccess()) {
-//     $_SESSION['message'] = $useCaseOutput->message();
-//     redirect('./signin.php');
-// }  else {
-//     $_SESSION['errors'][] = $useCaseOutput->message();
-//     Redirect::handler('./signup.php');
-// }
